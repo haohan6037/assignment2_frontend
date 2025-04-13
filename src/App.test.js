d45bb349f9b13aff85fa3eaff53c6ac6525db7db
gideonchencn <gideonchencn@gmail.com>
2025-04-13 22:32:46 +1200

    add some jest info

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
-import userEvent from '@testing-library/user-event';
-
+import '@testing-library/jest-dom';
 
 
 console.log("integration testing start")
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -8,4 +8,5 @@ class BlogsDataService {
     }
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default new BlogsDataService();
