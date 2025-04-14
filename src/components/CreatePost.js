769d8660fb0e4d0124baf307906b011b8087cc4f
haohan6037 <haohan6037@gmail.com>
2025-04-14 12:19:43 +1200

    create post

---
+++ src/components/CreatePost.js
@@ -0,0 +1,109 @@
+import React, {useEffect, useState} from "react";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+// import {useNavigate} from "react-router-dom";
+import axios from "axios";
+import BlogsDataService from "../services/blogs";
+
+const CreatePost = (props) => {
+    const [title, setTitle] = React.useState('');
+    const [content, setContent] = React.useState('');
+    const [category, setCategory] = React.useState('Tech');
+    // const navigate = useNavigate();
+    const [token, setToken] = useState("")
+    // const [post, setPost] = useState({})
+    const [err, setErr] = useState()
+
+    useEffect(() => {
+        const savedToken = localStorage.getItem('token');
+        setToken(savedToken)
+    }, []);
+
+    function handleTitleChange(event) {
+        setTitle(event.target.value);
+    }
+
+    function handleContentChange(event) {
+        setContent(event.target.value);
+    }
+
+    function handleCategoryChange(event) {
+        setCategory(event.target.value);
+    }
+
+    const handleCreate = () => {
+        let data = JSON.stringify({
+            "title": title,
+            "content": content,
+            "category": category
+        });
+
+        // let config = {
+        //     method: 'post',
+        //     maxBodyLength: Infinity,
+        //     url: 'http://localhost:8000/api/posts/create/',
+        //     headers: {
+        //         'Authorization': 'Token ' + token,
+        //         'Content-Type': 'application/json'
+        //     },
+        //     data: data
+        // };
+
+         BlogsDataService.createPost(data)
+            .then((response) => {
+                console.log(JSON.stringify(response.data));
+                setErr("Successfully created post")
+            })
+            .catch((error) => {
+                console.log(error);
+                setErr("Failed to create post")
+            });
+
+    };
+
+
+    return (
+        <Container>
+            <Form>
+                <Form.Group className="mb-3">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter title"
+                        value={title}
+                        onChange={handleTitleChange}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Content</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        rows={5}
+                        placeholder="Write your post content here"
+                        value={content}
+                        onChange={handleContentChange}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Category</Form.Label>
+                    <Form.Select value={category} onChange={handleCategoryChange}>
+                        <option value="Tech">Tech</option>
+                        <option value="Life">Life</option>
+                        <option value="Study">Study</option>
+                        <option value="Travel">Travel</option>
+                    </Form.Select>
+                </Form.Group>
+
+                <Button variant="primary" onClick={handleCreate}>
+                    Create Post
+                </Button>
+                <p>{err}</p>
+            </Form>
+        </Container>
+    );
+};
+
+export default CreatePost;
